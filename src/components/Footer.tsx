152851640b53c19b1ba50ee5108dd7471f3c2eb0
Marek <maswiecho@gmail.com>
2023-11-08 14:11:54 +0100

    0.2

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Layout from './Layout';
+import Link from 'next/link';
+
+export const Footer = () => {
+  return (
+    <footer className='w-full borded-t-2 border-solid border-dark font-medium text-lg'>
+      <Layout className='py-8 flex items-center justify-between'>
+        <span>{new Date().getFullYear()} &copy; All Rights Reserved. </span>
+        <div className='flex items-center'>
+          Build With{' '}
+          <span className='text-primary text-2xl px-1'> &#9825; </span>by &nbsp;
+          <span className='underline'>Marek</span>
+        </div>
+        <Link href='/'>Say hello</Link>
+      </Layout>
+    </footer>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,9 @@ import Head from 'next/head';
 import Image from 'next/image';
 import profilePic from '../../public/developer-pic-1.png';
 import AnimatedText from '@/components/AnimatedText';
+import Link from 'next/link';
+import { LinkArrow } from '../../public/LinkedArrow';
+import { Footer } from '@/components/Footer';
 
 export default function Home() {
   return (
@@ -26,14 +29,32 @@ export default function Home() {
                 className='!text-6xl !text-left'
                 text='Turning Vision Into Reality With Code And Design.'
               ></AnimatedText>
-              <p>
+              <p className='my-4 text-base font-medium'>
                 As a skilled full-stack developer, I am dedicated to turning
                 ideas into innovative web applications. Explore my latest
                 projects and articles, showcasing my expertise in React.js and
                 web development.
               </p>
+              <div className='flex items-center self-start mt-2'>
+                <Link
+                  className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark'
+                  href='/Web.pdf'
+                  target={'_blank'}
+                  download={true}
+                >
+                  Resume
+                  <LinkArrow className={'w-6 ml-1'}></LinkArrow>
+                </Link>
+                <Link
+                  className='ml-4 text-lg font-medium capitalize text-dark underline'
+                  href='mailto:marek.swiechowicz.linkedin@gmail.com'
+                >
+                  Contact
+                </Link>
+              </div>
             </div>
           </div>
+          {/* <Footer></Footer> */}
         </Layout>
       </main>
     </>
