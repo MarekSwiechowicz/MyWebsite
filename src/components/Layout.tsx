2af09251dbe4b75db69f87605e318576039eb78d
Marek <maswiecho@gmail.com>
2023-11-08 11:53:25 +0100

    0.1

---
+++ src/components/Layout.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+type LayoutProps = {
+  className?: string;
+  children: React.ReactNode;
+};
+
+const Layout: React.FC<LayoutProps> = ({ children, className = '' }) => {
+  return (
+    <div className={`w-full h-full inline-block z-0 bg-light p-32${className}`}>
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,25 +1,72 @@
 import Link from 'next/link';
 import React from 'react';
 import Logo from './Logo';
+import { useRouter } from 'next/router';
+import GithubIcon from '../../public/GithubIcon';
+import LinkedinIcon from '../../public/LinkedinIcon';
+import { motion } from 'framer-motion';
+
+type CustomLinkProps = {
+  href: string;
+  title: string;
+  className?: string;
+};
+
+const CustomLink: React.FC<CustomLinkProps> = ({
+  href,
+  title,
+  className = '',
+}) => {
+  const router = useRouter();
+  return (
+    <Link className={`${className} relative group`} href={href}>
+      {title}
+      <span
+        className={`h-[2px] inline-block  bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${
+          router.asPath === href ? 'w-full' : 'w-0'
+        }`}
+      >
+        &nbsp;
+      </span>
+    </Link>
+  );
+};
 
 const NavBar = () => {
   return (
     <div className='w-full px-32 py-8 font-medium flex items-center justify-between'>
       <nav>
-        <Link href='/'>Home</Link>
-        <Link href='/about'>About</Link>
-        <Link href='/projects'>Projects</Link>
-        <Link href='/articles'>Articles</Link>
+        <CustomLink href='/' title='Home' className='mr-4'></CustomLink>
+        <CustomLink href='/about' title='About' className='mx-4'></CustomLink>
+        <CustomLink
+          href='/projects'
+          title='Projects'
+          className='ml-4'
+        ></CustomLink>
       </nav>
-      <Logo></Logo>
-      <nav>
-        <Link href='/' target={'blank'}>
-          Github
-        </Link>
-        <Link href='/' target={'blank'}>
-          LinkedIn
-        </Link>
+      <nav className='flex items-center justify-center flex-wrap'>
+        <motion.a
+          whileHover={{ y: -2 }}
+          className='w-6 mx-3'
+          whileTap={{ scale: 0.9 }}
+          href='https://github.com'
+          target={'blank'}
+        >
+          <GithubIcon></GithubIcon>
+        </motion.a>
+        <motion.a
+          whileHover={{ y: -2 }}
+          className='w-6 mx-3'
+          whileTap={{ scale: 0.9 }}
+          href='https://twitter.com'
+          target={'blank'}
+        >
+          <LinkedinIcon></LinkedinIcon>
+        </motion.a>
       </nav>
+      <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
+        <Logo></Logo>
+      </div>
     </div>
   );
 };
