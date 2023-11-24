885f27bab9667738727e8def0ad35858527ddfb3
Marek <maswiecho@gmail.com>
2023-11-24 14:04:56 +0100

    footer ML

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'pl'], // Add other locales you are supporting
+    locales: ['en', 'pl'],
+    localeDetection: false,
   },
 };
