71d95571c148c6331f395e2af0fe52b0277c8e61
maswiecho <maswiecho@gmail.com>
2023-12-18 17:02:02 +0100

    project

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,14 +1,9 @@
 import React from "react";
-import SunIcon from "../../public/SunIcon";
-import MoonIcon from "../../public/MoonIcon";
 
 export const Projects = () => {
   return (
     <div>
       <div>123</div>
-      <div>123</div>
-      <SunIcon></SunIcon>
-      <MoonIcon></MoonIcon>
     </div>
   );
 };
