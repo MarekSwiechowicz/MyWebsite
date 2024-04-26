51dffa46f4182430c0dc562ac894161b4f74d860
maswiecho <maswiecho@gmail.com>
2024-04-26 11:39:02 +0200

    small word fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>{t("page_title", "Marek Święchowicz - Web Developer")}</title>
+        <title>{t("page_title", "Marek Święchowicz - Test Engineer")}</title>
         <meta
           name="description"
           content="Explore the creative portfolio of Marek Święchowicz, a professional web developer specializing in modern, user-friendly web solutions."
