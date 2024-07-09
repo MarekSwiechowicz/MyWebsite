1d61968c013ba1a9edd206d616c6cd65b0f591a0
Marek Swiechowicz <maswiecho@gmail.com>
2024-07-09 23:15:14 +0200

    update

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -102,7 +102,7 @@ const About = () => {
               </div>
               <div className=' flex flex-col xl:items-end justify-center items-center'>
                 <span className=' inline-block text-4xl md:text-7xl font-bold'>
-                  <AnimatedNumbers value={3}></AnimatedNumbers>+
+                  <AnimatedNumbers value={4}></AnimatedNumbers>+
                 </span>
                 <h2 className=' text-xxs sm:text-xl font-medium capitalize text-dark/75 dark:text-light/75'>
                   {t('years_of_experience_heading')}
