a64e999b5f8f4eba28136a94a5dab4c800b04949
maswiecho <maswiecho@gmail.com>
2024-07-29 17:40:17 +0200

    eslint + new photo

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { motion } from 'framer-motion';
-import { useTranslation } from 'next-i18next';
+import React from "react";
+import { motion } from "framer-motion";
+import { useTranslation } from "next-i18next";
 
 type SkillProps = {
   name: string;
@@ -12,9 +12,9 @@ const Skill: React.FC<SkillProps> = ({ name, x, y }) => {
   return (
     <motion.div
       whileHover={{ scale: 1.1 }}
-      className='absolute cursor-pointer flex items-center
+      className="absolute cursor-pointer flex items-center
        rounded-full font-semibold bg-transparent sm:bg-dark text-dark sm:text-light  py-2 lg:py-3 px-4 lg:px-6
-        shadow-dark dark:text-light dark:sm:text-dark dark:bg-transparent sm:dark:bg-light'
+        shadow-dark dark:text-light dark:sm:text-dark dark:bg-transparent sm:dark:bg-light"
       initial={{ x: 0, y: 0 }}
       whileInView={{ x: x, y: y, transition: { duration: 1.5 } }}
       viewport={{ once: true }}
@@ -25,33 +25,33 @@ const Skill: React.FC<SkillProps> = ({ name, x, y }) => {
 };
 
 export const Skills = () => {
-  const { t } = useTranslation('common');
+  const { t } = useTranslation("common");
 
   return (
     <>
-      <h2 className='font-bold text-5xl mb-4 sm:text-8xl mt-32 sm:mt-64 w-full text-center'>
-        {t('skills')}
+      <h2 className="font-bold text-5xl mb-4 sm:text-8xl mt-32 sm:mt-64 w-full text-center">
+        {t("skills")}
       </h2>
       <div
-        className='w-full h-[50vh] sm:h-[80vh] lg:h-[100vh] mb-32 relative flex items-center
-       justify-center rounded-full bg-circularLightSm sm:bg-circularLightLg lg:bg-circularLightXl dark:bg-circularDarkSm sm:dark:bg-circularDarkLg lg:dark:bg-circularDarkXl '
+        className="w-full h-[50vh] sm:h-[80vh] lg:h-[100vh] mb-32 relative flex items-center
+       justify-center rounded-full bg-circularLightSm sm:bg-circularLightLg lg:bg-circularLightXl dark:bg-circularDarkSm sm:dark:bg-circularDarkLg lg:dark:bg-circularDarkXl "
       >
         <motion.div
           whileHover={{ scale: 1.1 }}
-          className='cursor-pointer flex items-center rounded-full
+          className="cursor-pointer flex items-center rounded-full
            font-semibold  bg-dark text-light p-2  md:p-6 lg:p-8
-            shadow-dark dark:text-dark dark:bg-light'
+            shadow-dark dark:text-dark dark:bg-light"
         >
           Testing
         </motion.div>
-        <Skill name='SQL' x='-18vw' y='2vw'></Skill>
-        <Skill name='Python' x='-8vw' y='-10vw'></Skill>
-        <Skill name='JavaScript' x='21vw' y='6vw'></Skill>
-        <Skill name='CI/CD' x='0vw' y='11vw'></Skill>
-        <Skill name='Postman' x='-20vw' y='-9vw'></Skill>
-        <Skill name='Python' x='23vw' y='-5vw'></Skill>
-        <Skill name='Jest' x='0vw' y='-19vw'></Skill>
-        <Skill name='Selenium' x='10vw' y='-14vw'></Skill>
+        <Skill name="SQL" x="-18vw" y="2vw"></Skill>
+        <Skill name="Python" x="-8vw" y="-10vw"></Skill>
+        <Skill name="JavaScript" x="21vw" y="6vw"></Skill>
+        <Skill name="CI/CD" x="0vw" y="11vw"></Skill>
+        <Skill name="Postman" x="-20vw" y="-9vw"></Skill>
+        <Skill name="SQL" x="23vw" y="-5vw"></Skill>
+        <Skill name="Jest" x="0vw" y="-19vw"></Skill>
+        <Skill name="Selenium" x="10vw" y="-14vw"></Skill>
       </div>
     </>
   );
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,18 +1,18 @@
 /* eslint-disable react/no-unescaped-entities */
-import AnimatedText from '@/components/AnimatedText';
-import Layout from '@/components/Layout';
-import Head from 'next/head';
-import React, { useEffect, useRef } from 'react';
-import profilePic from '../../public/muzeum solo.jpg';
-import Image from 'next/image';
-import { useInView, useMotionValue, useSpring } from 'framer-motion';
-import { Skills } from '@/components/Skills';
-import { Experience } from '@/components/Experience';
-import { Education } from '@/components/Education';
-import { TransitionEffect } from '@/components/TransitionEffect';
-import { GetStaticProps } from 'next';
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import { useTranslation } from 'next-i18next';
+import AnimatedText from "@/components/AnimatedText";
+import Layout from "@/components/Layout";
+import Head from "next/head";
+import React, { useEffect, useRef } from "react";
+import profilePic from "../../public/dalle_developer.png";
+import Image from "next/image";
+import { useInView, useMotionValue, useSpring } from "framer-motion";
+import { Skills } from "@/components/Skills";
+import { Experience } from "@/components/Experience";
+import { Education } from "@/components/Education";
+import { TransitionEffect } from "@/components/TransitionEffect";
+import { GetStaticProps } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
 
 interface AnimatedNumbersProps {
   value: number;
@@ -31,7 +31,7 @@ const AnimatedNumbers: React.FC<AnimatedNumbersProps> = ({ value }) => {
   }, [isInView, value, motionValue]);
 
   useEffect(() => {
-    const unsubscribe = springValue.on('change', (latest) => {
+    const unsubscribe = springValue.on("change", (latest) => {
       if (ref.current && latest.toFixed(0) <= value) {
         ref.current.textContent = latest.toFixed(0);
       }
@@ -44,68 +44,68 @@ const AnimatedNumbers: React.FC<AnimatedNumbersProps> = ({ value }) => {
 };
 
 const About = () => {
-  const { t } = useTranslation('common'); // Initialize useTranslation
+  const { t } = useTranslation("common"); // Initialize useTranslation
 
   return (
     <>
       <Head>
-        <title>{t('page_title', 'About')}</title>
-        <meta name='description' content='any description'></meta>
+        <title>{t("page_title", "About")}</title>
+        <meta name="description" content="any description"></meta>
       </Head>
       <TransitionEffect />
-      <main className='flex w-full flex-col items-center justify-center dark:text-light'>
-        <Layout className=''>
+      <main className="flex w-full flex-col items-center justify-center dark:text-light">
+        <Layout className="">
           <AnimatedText
-            className='mb-8 sm:mb-16 text-4xl sm:text-7xl lg:text-8xl w-full items-center '
-            text={t('passion_fuels_purpose')}
+            className="mb-8 sm:mb-16 text-4xl sm:text-7xl lg:text-8xl w-full items-center "
+            text={t("passion_fuels_purpose")}
           ></AnimatedText>
 
-          <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-8 sm:gap-16'>
-            <div className='order-2 xl:col-span-3 sm:order-none '>
-              <h2 className='mb-4 mt-5 sm:mt-0 text-lg font-bold uppercase text-dark/75 dark:text-light/75'>
-                {t('biography_heading')}
+          <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-8 sm:gap-16">
+            <div className="order-2 xl:col-span-3 sm:order-none ">
+              <h2 className="mb-4 mt-5 sm:mt-0 text-lg font-bold uppercase text-dark/75 dark:text-light/75">
+                {t("biography_heading")}
               </h2>
 
-              <p className=' xxl:text-2xl font-medium '>
-                {t('biography_paragraph_1')}
+              <p className=" xxl:text-2xl font-medium ">
+                {t("biography_paragraph_1")}
               </p>
-              <p className=' xxl:text-2xl my-4 font-medium'>
-                {t('biography_paragraph_2')}
+              <p className=" xxl:text-2xl my-4 font-medium">
+                {t("biography_paragraph_2")}
               </p>
             </div>
-            <div className='order-1 xl:col-span-3 sm:order-none p-8 relative h-max rounded-2xl border-2 border-solid border-dark bg-light dark:bg-dark dark:border-light'>
-              <div className='absolute top-1 -right-3 -z-10 w-[102%] h-[103%] rounded-3xl  bg-dark dark:bg-light' />
+            <div className="order-1 xl:col-span-3 sm:order-none p-8 relative h-max rounded-2xl border-2 border-solid border-dark bg-light dark:bg-dark dark:border-light">
+              <div className="absolute top-1 -right-3 -z-10 w-[102%] h-[103%] rounded-3xl  bg-dark dark:bg-light" />
               <Image
                 src={profilePic}
-                alt={t('profile_pic_alt')}
-                className='w-full h-auto rounded-2xl'
+                alt={t("profile_pic_alt")}
+                className="w-full h-auto rounded-2xl"
                 priority
-                sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
               />
             </div>
-            <div className='order-3 sm:col-span-2 xl:col-span-2 xl:flex xl:flex-col grid grid-cols-3 justify-between'>
-              <div className='flex flex-col xl:items-end justify-center items-center'>
-                <span className=' inline-block text-4xl md:text-7xl font-bold'>
+            <div className="order-3 sm:col-span-2 xl:col-span-2 xl:flex xl:flex-col grid grid-cols-3 justify-between">
+              <div className="flex flex-col xl:items-end justify-center items-center">
+                <span className=" inline-block text-4xl md:text-7xl font-bold">
                   <AnimatedNumbers value={10}></AnimatedNumbers>+
                 </span>
-                <h2 className=' text-xxs sm:text-xl font-medium capitalize text-dark/75 dark:text-light/75'>
-                  {t('satisfied_clients_heading')}
+                <h2 className=" text-xxs sm:text-xl font-medium capitalize text-dark/75 dark:text-light/75">
+                  {t("satisfied_clients_heading")}
                 </h2>
               </div>
-              <div className=' flex flex-col xl:items-end justify-center items-center'>
-                <span className=' inline-block text-4xl md:text-7xl font-bold'>
+              <div className=" flex flex-col xl:items-end justify-center items-center">
+                <span className=" inline-block text-4xl md:text-7xl font-bold">
                   <AnimatedNumbers value={20}></AnimatedNumbers>+
                 </span>
-                <h2 className=' text-xxs sm:text-xl font-medium capitalize text-dark/75 dark:text-light/75'>
-                  {t('projects_completed_heading')}
+                <h2 className=" text-xxs sm:text-xl font-medium capitalize text-dark/75 dark:text-light/75">
+                  {t("projects_completed_heading")}
                 </h2>
               </div>
-              <div className=' flex flex-col xl:items-end justify-center items-center'>
-                <span className=' inline-block text-4xl md:text-7xl font-bold'>
+              <div className=" flex flex-col xl:items-end justify-center items-center">
+                <span className=" inline-block text-4xl md:text-7xl font-bold">
                   <AnimatedNumbers value={4}></AnimatedNumbers>+
                 </span>
-                <h2 className=' text-xxs sm:text-xl font-medium capitalize text-dark/75 dark:text-light/75'>
-                  {t('years_of_experience_heading')}
+                <h2 className=" text-xxs sm:text-xl font-medium capitalize text-dark/75 dark:text-light/75">
+                  {t("years_of_experience_heading")}
                 </h2>
               </div>
             </div>
@@ -123,11 +123,11 @@ const About = () => {
 export default About;
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  const locale = context.locale || 'en';
+  const locale = context.locale || "en";
 
   return {
     props: {
-      ...(await serverSideTranslations(locale, ['common'])),
+      ...(await serverSideTranslations(locale, ["common"])),
     },
   };
 };
