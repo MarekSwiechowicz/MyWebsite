fb10f507e22a2dacf771dd05254ee9494206ada8
Marek Święchowicz <maswiecho@gmail.com>
2024-11-05 14:23:09 +0100

    navbar callback

--- src/components/Navbar/NavBar.tsx
+++ src/components/Navbar/NavBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { twMerge } from "tailwind-merge";
 import { useTranslation } from "next-i18next";
 import MoonIcon from "../../../public/MoonIcon";
@@ -14,29 +14,31 @@ const NavBar = () => {
     setIsOpen(!isOpen);
   };
 
-  const closeOnOutsideClick = (event: MouseEvent) => {
-    const modal = document.getElementById("modalId");
-    const button = document.getElementById("buttonId");
-    if (
-      isOpen &&
-      modal &&
-      button &&
-      !modal.contains(event.target as Node) &&
-      !button.contains(event.target as Node)
-    ) {
-      setIsOpen(false);
-    }
-  };
+  const closeOnOutsideClick = useCallback(
+    (event: MouseEvent) => {
+      const modal = document.getElementById("modalId");
+      const button = document.getElementById("buttonId");
+      if (
+        isOpen &&
+        modal &&
+        button &&
+        !modal.contains(event.target as Node) &&
+        !button.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    },
+    [isOpen]
+  );
 
   useEffect(() => {
     if (isOpen) {
       document.addEventListener("mousedown", closeOnOutsideClick);
     }
-
     return () => {
       document.removeEventListener("mousedown", closeOnOutsideClick);
     };
-  }, [isOpen]);
+  }, [isOpen, closeOnOutsideClick]);
 
   const Links = [
     { href: "/", title: "home", className: "mr-4" },
@@ -51,32 +53,32 @@ const NavBar = () => {
     >
       <button
         id="buttonId"
-        className=" flex lg:hidden flex-col justify-center items-center"
+        className="flex lg:hidden flex-col justify-center items-center"
         onClick={handleClick}
       >
         <span
-          className={`bg-dark dark:bg-light transition-all duration-300 ease-out block h-0.5 w-6 rounded-sm  ${
+          className={`bg-dark dark:bg-light transition-all duration-300 ease-out block h-0.5 w-6 rounded-sm ${
             isOpen ? "rotate-45 translate-y-1" : "-translate-y-0.5"
           }`}
         ></span>
         <span
-          className={`bg-dark dark:bg-light transition-all duration-300 ease-out block h-0.5 w-6
-     rounded-sm my-0.5 ${isOpen ? "opacity-0" : "opacity-100"}`}
+          className={`bg-dark dark:bg-light transition-all duration-300 ease-out block h-0.5 w-6 rounded-sm my-0.5 ${
+            isOpen ? "opacity-0" : "opacity-100"
+          }`}
         ></span>
         <span
-          className={`bg-dark dark:bg-light transition-all duration-300 ease-out block h-0.5 w-6 rounded-sm  ${
+          className={`bg-dark dark:bg-light transition-all duration-300 ease-out block h-0.5 w-6 rounded-sm ${
             isOpen ? "-rotate-45 -translate-y-1" : "translate-y-0.5"
           }`}
         ></span>
       </button>
-      {/* workaround about moonicon doesnt show correctly  */}
-      <div className=" opacity-0">
-        <MoonIcon></MoonIcon>
+      <div className="opacity-0">
+        <MoonIcon />
       </div>
-      <DesktopNav></DesktopNav>
+      <DesktopNav />
       <MobileNavModal isOpen={isOpen} handleClick={handleClick} />
       <div className="absolute left-[50%] top-2 translate-x-[-50%]">
-        <Logo></Logo>
+        <Logo />
       </div>
     </header>
   );
