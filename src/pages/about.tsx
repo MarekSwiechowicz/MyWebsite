b7f9b876630f3cb7b96bc0ac1d738e4e887af592
maswiecho <maswiecho@gmail.com>
2025-06-10 22:30:12 +0100

    mirroredPhoto

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,7 +3,7 @@ import AnimatedText from "@/components/AnimatedText";
 import Layout from "@/components/Layout";
 import Head from "next/head";
 import React, { useEffect, useRef } from "react";
-import profilePic from "../../public/profilePicture.png";
+import profilePic from "../../public/marekMirror.png";
 import Image from "next/image";
 import { useInView, useMotionValue, useSpring } from "framer-motion";
 import { Skills } from "@/components/Skills";
