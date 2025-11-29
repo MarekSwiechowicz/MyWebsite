9fbac4befa78513ffb92fd2a56d98359116d4ebd
maswiecho <maswiecho@gmail.com>
2025-11-29 23:09:12 +0100

    build fix

--- tests/page-objects/AboutPage.ts
+++ tests/page-objects/AboutPage.ts
@@ -2,11 +2,7 @@ import { Page } from "@playwright/test";
 import { BasePage } from "./BasePage";
 
 export class AboutPage extends BasePage {
-  constructor(page: Page) {
-    super(page);
-  }
-
   async goto() {
-    await this.gotoPath("/about");
+    await super.goto("/about");
   }
 }
--- tests/page-objects/ProjectsPage.ts
+++ tests/page-objects/ProjectsPage.ts
@@ -2,11 +2,7 @@ import { Page } from "@playwright/test";
 import { BasePage } from "./BasePage";
 
 export class ProjectsPage extends BasePage {
-  constructor(page: Page) {
-    super(page);
-  }
-
   async goto() {
-    await this.gotoPath("/projects");
+    await super.goto("/projects");
   }
 }
