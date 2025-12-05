217f4929bb6f644a3faac241e3138def8df6d423
Swiechowicz, Marek <Marek.Swiechowicz@verisk.com>
2025-12-05 13:59:59 +0100

    warning fix

--- src/components/FacebookMsg.tsx
+++ src/components/FacebookMsg.tsx
@@ -5,7 +5,7 @@ import { FACEBOOK_CONFIG } from "@/constants/config";
 export default function FacebookMsg() {
   return (
     <FacebookProvider appId={FACEBOOK_CONFIG.APP_ID} chatSupport>
-      <CustomChat pageId={FACEBOOK_CONFIG.PAGE_ID} minimized={false} />
+      <CustomChat pageId={FACEBOOK_CONFIG.PAGE_ID} />
     </FacebookProvider>
   );
 }
